45848be8c9c348da1d0cc24066ba09ff15c9aec3
Silas Krause <silas.krause@odt.net>
2019-03-28 13:56:50 +0100

    refactoring to match es6 spec
    -method overloading
    -add source-width and sx

--- draft.js
+++ draft.js
@@ -4,26 +4,52 @@ class GIF extends Image {
   }
 }
 
-CanvasRenderingContext2D.prototype.drawGIF=function(img, width, height, numberOfFrames = 1, repeat = true){ 
-  let frameIndex = 0;
-  update = ()=>{
+CanvasRenderingContext2D.prototype.drawGIF=function(img, a, b, c, d, e, f, g, h, i, j){
+  let sx=0, sy=0, sWidth, sHeight, dx, dy, dWidth, dHeight, numberOfFrames, repeat=true, width, height, frameIndex=0;
+  const l = arguments.length;
+  img.onload = function(){
+    width = img.width;
+    height = img.height;
+    if (!dWidth || !dHeight){dWidth = width; dHeight = height};
+    if (!sWidth || !sHeight){sWidth = width / numberOfFrames; sHeight = height};
+  }
+  switch(true){  
+    case (l<=5):
+        [, dx, dy] = arguments;
+        check(c,d);
+        break;
+    case (l>5 && l<=7):
+        [, dx, dy, dWidth, dHeight] = arguments;
+        check(e,f);
+        break;
+    case (l>7 && l<=11):
+        [,sx,sy,sWidth,sHeight,dx,dy,dWidth,dHeight] = arguments;
+        check(i,j)
+        break;
+    default: console.log('error');
+  }
+  function check(f,r){
+    numberOfFrames = (typeof f === 'number') ? f : 1;
+    if(r === false) repeat = false;
+  }
+  function loop(){
+    update(); render();
+    setTimeout(() => {
+      window.requestAnimationFrame(loop)
+    }, 100)
+  }
+  function update(){
     if (frameIndex < numberOfFrames - 1 && repeat) {frameIndex += 1;}
     else {frameIndex = 0;}
   };
   render = ()=>{
     this.drawImage(
-      image,
-      frameIndex * width / numberOfFrames, 0,
-      width / numberOfFrames, height,
-      0, 0,
-      width / numberOfFrames, height
+      img,
+      (frameIndex * width + sx) / numberOfFrames, 0+sy,
+      sWidth, sHeight,
+      dx, dy,
+      dWidth / numberOfFrames, dHeight
     );  
-  }
-  function loop() {
-    setTimeout(() => {
-    window.requestAnimationFrame(loop)
-    update(); render();
-    }, 100)
   }
   img.addEventListener("load", loop);
 };
