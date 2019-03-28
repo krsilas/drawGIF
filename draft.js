dbb4a48dd7aa049edd641df3b49d8e6ab87bfa8b
Silas Krause <silas.krause@odt.net>
2019-03-28 09:09:59 +0100

    v0.1

--- draft.js
+++ draft.js
@@ -1,54 +1,46 @@
-
-this._canvas = ReactDOM.findDOMNode(this);
-this._context = this._canvas.getContext('2d');
-
-
-  _sprite(options) {	
-    var that = {},
-    frameIndex = 0,
-    tickCount = 0,
-    ticksPerFrame = 2,
-    numberOfFrames = options.numberOfFrames || 1;
-    that.context = options.context;
-    that.width = options.width;
-    that.height = options.height;
-    that.image = options.image;
-    that.loop = options.loop;
-    that.rend = ()=>{
-      that.context.drawImage(
-        that.image,
-        frameIndex * that.width / numberOfFrames, 0,
-        that.width / numberOfFrames, that.height,
-        500, 200,
-        that.width / numberOfFrames, that.height
-      );   
+class GIF {
+  constructor(){
+    this.src=null;
+  }
+}
+class Sprite extends GIF{
+  constructor(options = {}){
+    super();
+    let frameIndex = 0;
+    const numberOfFrames = options.numberOfFrames || 1;
+    this.context = options.context;
+    this.width = options.width;
+    this.height = options.height;
+    this.image = options.image;
+    this.loop = (options.loop === false) ? false : true;
+    this.render = ()=>{
+      this.context.drawImage(
+        this.image,
+        frameIndex * this.width / numberOfFrames, 0,
+        this.width / numberOfFrames, this.height,
+        0, 0,
+        this.width / numberOfFrames, this.height
+      );  
     };
-    that.update = ()=>{
-      tickCount += 1;
-      if (tickCount > ticksPerFrame) {
-        tickCount = 0;
-        // Go to the next frame
-        if (frameIndex < numberOfFrames - 1) {frameIndex += 1;}
-        else if (that.loop) {frameIndex = 0; return 0;}
-        else {return -1;}
-      }
-    }; 
-    return that;
+    this.update = ()=>{
+      if (frameIndex < numberOfFrames - 1) {frameIndex += 1;}
+      else {frameIndex = 0;}
+  }; 
   }
+}
 
-  _gif(url, width, height, numberOfFrames){
-    setTimeout(()=>{
-      var image = new Image();
-      image.src = `${url}`;
-      let GIFo = this._sprite({
-        context: this._context,
-        width, height, image, numberOfFrames
-      });
-      function gifLoop() {
-        if (GIFo.update() !== -1) {window.requestAnimationFrame(gifLoop)}
-        GIFo.update(); GIFo.rend();
-      }
-      image.addEventListener("load", gifLoop);      
-    }, 0);
+CanvasRenderingContext2D.prototype.drawGif=function(img, width, height, numberOfFrames){ 
+  const image = new Image();
+  image.src = img.src;
+  const GIFo = new Sprite({
+    context: this,
+    width, height, image, numberOfFrames
+  });
+  function loop() {
+    setTimeout(() => {
+    window.requestAnimationFrame(loop)
+    GIFo.update(); GIFo.render();
+    }, 100)
   }
-  
+  image.addEventListener("load", loop);
+};
